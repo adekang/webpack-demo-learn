395eaab3c5b4187f937bbb4e779dfcca6b3006e0
adekang <adekang@163.com>
2021-11-04 09:38:24 +0800

    添加基础配置

---
+++ webpack.coonfig.js
@@ -0,0 +1,12 @@
+const path = require('path'); // node.js 的路径模块
+
+module.exports = {
+  // entry: './src/index.js', // 入口文件（简写形式）
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'), // 打包后的路径
+    filename: 'bundle.js', // 打包后的文件名
+  }
+}
