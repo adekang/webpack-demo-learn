c8ce7175cfc2459158c33c1fb7798593f8750871
adekang <adekang@163.com>
2021-11-04 09:41:40 +0800

    添加开发模式

--- webpack.coonfig.js
+++ webpack.coonfig.js
@@ -1,6 +1,7 @@
 const path = require('path'); // node.js 的路径模块
 
 module.exports = {
+  mode: 'development', // 'development' | 'production'
   // entry: './src/index.js', // 入口文件（简写形式）
   entry: {
     main: './src/index.js',
