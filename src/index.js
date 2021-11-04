09af661758de9d919c7ccec6a3d22eea77f2b1a6
adekang <adekang@163.com>
2021-11-04 10:32:51 +0800

    处理sass

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const sea = require('./assets/images/sea.svg')
 
 const root = document.getElementById('root')
 
+import "./assets/styles/reset.scss";
+
 // 插入一张图片
 const img = new Image()
 img.src = boat // 图片模块 dog1 作为 img 变量的 src 属性值
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path') // node.js 的路径模块
-const miniSVGDataURI = require("mini-svg-data-uri"); // 最小化svg
+const miniSVGDataURI = require('mini-svg-data-uri') // 最小化svg
 module.exports = {
 	mode: 'development', // 'development' | 'production'
 	// entry: './src/index.js', // 入口文件（简写形式）
@@ -9,8 +9,8 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'), // 打包后的路径
 		filename: 'bundle.js', // 打包后的文件名
-       // 静态文件打包后的路径及文件名（默认是走全局的，如果有独立的设置就按照自己独立的设置来。）
-       assetModuleFilename: "assets/[name]_[hash][ext]",
+		// 静态文件打包后的路径及文件名（默认是走全局的，如果有独立的设置就按照自己独立的设置来。）
+		assetModuleFilename: 'assets/[name]_[hash][ext]',
 	},
 	// *** 模块选项中匹配的文件会通过 loaders 来转换！
 	module: {
@@ -19,44 +19,61 @@ module.exports = {
 			{
 				test: /\.(jpe?g|png|gif|svg)$/i,
 				type: 'asset', // 一般会转换为 "asset/resource"
-        generator: {
-          filename: "images/[name]_[hash][ext]", // 独立的配置
-        },
+				generator: {
+					filename: 'images/[name]_[hash][ext]', // 独立的配置
+				},
 				parser: {
 					dataUrlCondition: {
-						maxSize: 8 * 1024 // 8kb （低于8kb都会压缩成 base64）
-					}
+						maxSize: 8 * 1024, // 8kb （低于8kb都会压缩成 base64）
+					},
 				},
-      },
+			},
 			// 字体文件
 			{
 				test: /\.(otf|eot|woff2?|ttf|svg)$/i,
 				type: 'asset', // 一般会转换为 "asset/inline"
-        generator: {
-          filename: "fonts/[name]_[hash][ext]",
-        },
-      },
+				generator: {
+					filename: 'fonts/[name]_[hash][ext]',
+				},
+			},
 			// 数据文件
 			{
 				test: /\.(txt|xml)$/i,
 				type: 'asset/source', // 一般会转换成 "asset/source"
 			},
-      // svg文件
-      {
-        test: /\.svg$/i,
-        type: "asset",
-        generator: {
-          dataUrl(content) {
-            content = content.toString();
-            return miniSVGDataURI(content);
-          },
-        },
+			// svg文件
+			{
+				test: /\.svg$/i,
+				type: 'asset',
+				generator: {
+					dataUrl(content) {
+						content = content.toString()
+						return miniSVGDataURI(content)
+					},
+				},
 				parser: {
 					dataUrlCondition: {
-						maxSize: 2 * 1024 // 2kb （低于2kb都会压缩）
-					}
+						maxSize: 2 * 1024, // 2kb （低于2kb都会压缩）
+					},
 				},
-      },
+			},
+			// css配置
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			// scss配置
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					'style-loader',
+					// Translates CSS into CommonJS
+					'css-loader',
+					// Compiles Sass to CSS
+					'sass-loader',
+				],
+			},
 		],
 	},
 }
