b210bef3e8a1d023fd0dc5ddaf16d05e79e21208
adekang <adekang@163.com>
2021-11-05 11:13:54 +0800

    实现不同打包路径

---
+++ build/webpack.common.config.js
@@ -0,0 +1,104 @@
+// 公共配置
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // 入口文件
+  entry: './src/index.js',
+  // 输出文件
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    assetModuleFilename: 'assets/[name]_[hash][ext]',
+    clean: true,
+  },
+  // 模块
+  module: {
+    rules: [
+			//  处理es6+
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			// 图片文件
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				type: 'asset', // 一般会转换为 "asset/resource"
+				generator: {
+					filename: 'images/[name]_[hash][ext]', // 独立的配置
+				},
+				parser: {
+					dataUrlCondition: {
+						maxSize: 8 * 1024, // 8kb （低于8kb都会压缩成 base64）
+					},
+				},
+			},
+			// 字体文件
+			{
+				test: /\.(otf|eot|woff2?|ttf|svg)$/i,
+				type: 'asset', // 一般会转换为 "asset/inline"
+				generator: {
+					filename: 'fonts/[name]_[hash][ext]',
+				},
+			},
+			// 数据文件
+			{
+				test: /\.(txt|xml)$/i,
+				type: 'asset/source', // 一般会转换成 "asset/source"
+			},
+			// svg文件
+			{
+				test: /\.svg$/i,
+				type: 'asset',
+				generator: {
+					dataUrl(content) {
+						content = content.toString()
+						return miniSVGDataURI(content)
+					},
+				},
+				parser: {
+					dataUrlCondition: {
+						maxSize: 2 * 1024, // 2kb （低于2kb都会压缩）
+					},
+				},
+			},
+			// css配置
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+				sideEffects: true // 这些文件不要 Tree Shaking。
+			},
+			// scss配置
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					'style-loader',
+					// Translates CSS into CommonJS
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 2,
+							// 0 => no loaders (default);
+							// 1 => postcss-loader;
+							// 2 => postcss-loader, sass-loader
+							modules: true, // 默认是 false ***
+						},
+					},
+					// Compiles Sass to CSS
+					'sass-loader',
+					'postcss-loader',
+				],
+			},
+		],
+  },
+  // 插件
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
---
+++ build/webpack.dev.config.js
@@ -0,0 +1,32 @@
+// webpack.dev.config.js
+const path = require('path');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge'); // 插件引入
+const commonConfig = require('./webpack.common.config'); // 引入共用配置
+
+const devConfig = {
+  // 模式
+  mode: 'development',
+  // 性能优化 (tree shaking)
+  optimization: {
+    usedExports: true
+  },
+  // source-map
+  devtool: 'eval-cheap-module-source-map',
+  // 开发服务器
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),// 指定被访问html页面所在目录的路径
+    },
+    compress: true,
+    port: 8888,
+    open: true,
+    hot: true // 代码变化后，自动刷新页面
+  },
+  // 插件
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
+
+module.exports = merge(commonConfig, devConfig); // 共用配置与开发配置合并
